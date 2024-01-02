f7063b80aac0f279083c594795973e38a73843cb
“[dfitzpatrick13]” <[danfitz156@gmail.com]>
2024-01-02 13:26:40 -0500

    psuedo code and basic structure

---
+++ mod2/src/NavBar.jsx
@@ -0,0 +1,16 @@
+const NavBar = () => {
+
+    return ( 
+        <nav className="navbar">
+            <h1>Pokemon Api Website</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/pokedex">Pokedex</a>
+                </div>
+        </nav>
+
+
+     );
+}
+ 
+export default NavBar;
