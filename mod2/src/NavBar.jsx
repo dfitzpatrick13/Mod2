d0a55db1df7cc4b1dab6f615ef7b1c7d17a446e3
“[dfitzpatrick13]” <[danfitz156@gmail.com]>
2024-01-04 11:40:18 -0500

    testing api

--- mod2/src/NavBar.jsx
+++ mod2/src/NavBar.jsx
@@ -1,3 +1,7 @@
+import Pokedex from "./Pokedex";
+
+
+
 const NavBar = () => {
 
     return ( 
--- mod2/src/Pokedex.jsx
+++ mod2/src/Pokedex.jsx
@@ -1,8 +1,46 @@
-const Pokedex = () => {
+import React,{useState} from "react";
 
+const Pokedex = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [pokemonData,setPokemonData] = useState(null)
+
+    const handleSearch = async () => {
+        try {
+          const response = await fetch('https://pokeapi-stefan-skliarov-v1.p.rapidapi.com/pokemon?name=' + searchTerm.toLowerCase(), {
+            headers: {
+              'x-rapidapi-key': '3558788312msh9e2a10b42f82567p197369jsn4412d29a977e',
+              'x-rapidapi-host': 'pokeapi-stefan-skliarov-v1.p.rapidapi.com',
+            },
+          });
+    
+          const data = await response.json();
+          setPokemonData(data);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+    
+      const handleInputChange = (event) => {
+        setSearchTerm(event.target.value);
+      };
+    
     return ( 
-        <h1>this is where the pokemon search page will be </h1>
-     );
+        <div>
+        <input
+          type="text"
+          placeholder="Search Pokemon"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSearch}>Search</button>
+  
+        {pokemonData && (
+          <div>
+            <h2>{pokemonData.name}</h2>
+            <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+          </div>
+        )}
+      </div>     );
 }
  
 export default Pokedex;
